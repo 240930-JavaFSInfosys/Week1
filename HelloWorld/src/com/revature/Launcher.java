186c28b0cc30d7c707c5d57b985953132579c442
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-10-01 09:30:28 -0400

    Added HelloWorld to Week1 Repo

---
+++ HelloWorld/src/com/revature/Launcher.java
@@ -0,0 +1,26 @@
+package com.revature;
+
+public class Launcher {
+
+    // This is the main method - the starting point for our application
+    public static void main(String[] args) {
+
+        // This is a print statement
+        // It allows us to "print" a message to the console (the box that appears when we run the app)
+        System.out.println("Hello World!");
+
+        System.out.println("We are learning fundamental Java!");
+
+        System.out.println("We can type \"sout\" to quickly generate a print statement!");
+        
+        /* This is a block comment AKA multi-line comment
+
+        System = This system. The console.
+        out = output, we're defining something that will be outputted.
+        println = prints a line to the console
+
+        fun fact: if we just did System.out.print, they would all print on the same line */
+
+    }
+
+}
