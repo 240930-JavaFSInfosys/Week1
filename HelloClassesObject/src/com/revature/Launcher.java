5c399ae05a9c6b36904bb165d176b8f050f2c5ac
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-10-02 11:25:12 -0400

    Explored constructors and class/object relationship

--- HelloClassesObject/src/com/revature/Launcher.java
+++ HelloClassesObject/src/com/revature/Launcher.java
@@ -1,10 +1,36 @@
 package com.revature;
 
+import com.revature.models.Animal;
+
 public class Launcher {
 
     public static void main(String[] args) {
 
-        //TODO: a bunch of stuff with Animal objects here
+        //Instantiate an Animal object with the no-args constructor
+        //This will give us a generic Animal object with generic values
+        Animal a = new Animal();
+
+        //let's use some Animal variables
+        System.out.println("Our animal has " + a.legs + " legs"); // 0 legs is no way to live
+
+        //we can assign new values to our object's variables
+        a.legs = 12;
+        System.out.println("Now our animal has " + a.legs + " legs");
+
+        //let's invoke the 2 animal methods
+        a.eat();
+
+        //since makeSound() returns a String, we need to print it out to see anything
+        System.out.println(a.makeSound());
+
+        //OK let's instantiate 2 new Animals using out all-args constructor
+        Animal a2 = new Animal(4, 2, "woof");
+        Animal a3 = new Animal(2, 5, "oo oo aa aa");
+
+        //use makeNoise() methods just for fun
+        System.out.println(a2.makeSound());
+        System.out.println(a3.makeSound());
+
 
     }
 
--- HelloClassesObject/src/com/revature/models/Animal.java
+++ HelloClassesObject/src/com/revature/models/Animal.java
@@ -17,12 +17,44 @@ public class Animal {
 
     //This is a method lets an Animal make a sound
     public String makeSound(){
-        return sound;
+        return sound; //Returns the value of the sound variable (which is a String)
     }
 
-    //TODO: Ben will add a method disambiguation just as a cheat sheet for how methods work
+    /* Method Disambiguation
 
-    //TODO: no-args constructor and an all-args constructor
+    The methods above are both public, so they're accessible throughout the entire app
+
+    Methods have RETURN TYPES - the type of data a method will return
+        -eat() has a void return type, meaning it doesn't return anything
+        -makeSound() has a String return type, meaning it will return a String
+
+        -Math.random() returns a double from 0.0 - 1.0
+
+    These methods have no (parameters). So we don't need to supply them any info */
+
+
+    //no-args constructor and an all-args constructor----------------/
+
+    //no-args constructor. It takes NO ARGuments. Often used to define our own default values
+    //If we instantiate a new Animal and give it no arguments, this constructor will be called
+    public Animal(){
+        this.legs = 0;
+        this.age = 1;
+        this.sound = "yawn";
+    }
+
+    //all-args constructor. It takes ALL available ARGuments for an Animal (every variable)
+    //Whatever values are supplied when calling this constructor will be assigned to the appropriate variables
+    public Animal(int legs, int age, String sound){
+        this.legs = legs;
+        this.age = age;
+        this.sound = sound;
+    }
+
+
+    //NOTE: If we don't define a constructor, Java will provide a no-args constructor for us with totally default values
+    //Once we define a custom constructor, this default no-args constructor goes away
+
 
     //TODO: subclass that extends this Animal Class
 
