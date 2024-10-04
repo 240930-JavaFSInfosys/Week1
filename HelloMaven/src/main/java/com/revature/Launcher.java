f93a5fd6d3317856c85fbcad7a5fe09aa1ac4773
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-10-04 15:15:52 -0400

    created a base maven project

---
+++ HelloMaven/src/main/java/com/revature/Launcher.java
@@ -0,0 +1,9 @@
+package com.revature;
+
+public class Launcher {
+
+    //TODO: this demo will show testing with JUnit and logging with Logback
+
+    //TODO: I'll make a simple class (calculator?) that we can test and log
+
+}
